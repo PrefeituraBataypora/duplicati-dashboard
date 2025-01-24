7913405ea17c9d45330a4157eea0aac4b71d98f8
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-01-24 11:50:42 -0300

    feat: setup react query

---
+++ src/providers/query.tsx
@@ -0,0 +1,10 @@
+import { queryClient } from "@/lib/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
+
+const QueryProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export { QueryProvider };
