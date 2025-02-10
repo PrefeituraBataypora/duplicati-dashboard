d9a6c9159b30d8a843be796f10d48f52376e5689
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2025-02-10 10:48:22 -0300

    feat: add DeleteDevice component for device deletion with confirmation dialog

---
+++ src/components/device/delete/index.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { deleteDevice } from "@/actions/device/delete";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { useQueryClient } from "@tanstack/react-query";
+import { Trash2 } from "lucide-react";
+import { toast } from "sonner";
+
+interface DeleteDeviceProps {
+  id: string;
+  ip: string;
+}
+
+const DeleteDevice = ({ id, ip }: DeleteDeviceProps) => {
+  const queryClient = useQueryClient();
+
+  const handleDelete = async () => {
+    try {
+      await deleteDevice({ id });
+      toast.success("Dispositivo excluído com sucesso");
+      queryClient.invalidateQueries({ queryKey: ["devices"] });
+    } catch (error) {
+      console.error(error);
+      toast.error("Ocorreu um erro ao excluir o dispositivo");
+    }
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive" size="icon">
+          <Trash2 />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Deseja realmente excluir o dispositivo {ip} ?
+          </AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDelete}>Excluir</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export { DeleteDevice };
